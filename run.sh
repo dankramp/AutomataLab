3f8334d2ff5968090f7625d7a7c94c940b224794
dankramp <danielbkramp@gmail.com>
2017-08-23 14:40:57 -0400

    Updated run.sh to link config, set env variable

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-./VASimVis --docroot . --http-address 0.0.0.0 --http-port 9090
+
+LD_LIBRARY_PATH=/VASim/MNRL/C++:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH
+
+./VASimVis --docroot . --http-address 0.0.0.0 --http-port 9090 --config wt_config.xml
